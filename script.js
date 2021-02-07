da3f987be9bf683e8b275cddff4c0f5866cd0783
Mateusz_Walowski <mat.walowski@gmail.com>
2021-02-07 12:44:46 +0100

    calculating&scrolling

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 window.addEventListener("load", start);
+document.querySelector("#clear").addEventListener("click", clear);
 
 var mathresults = [];
 document.querySelector("#results > p").textContent = mathresults;
@@ -11,6 +12,18 @@ function start() {
 }
 
 function calculate() {
+  if (document.querySelector("#operator").value == "add") {
+    adding();
+  } else if (document.querySelector("#operator").value == "sub") {
+    substracting();
+  } else if (document.querySelector("#operator").value == "mul") {
+    multiplying();
+  } else if (document.querySelector("#operator").value == "div") {
+    dividing();
+  }
+}
+
+function adding() {
   var firstnumber = Number(document.querySelector("#firstnumber").value);
   var secondnumber = Number(document.querySelector("#secondnumber").value);
   console.log("adding " + firstnumber + " + " + secondnumber);
@@ -18,4 +31,50 @@ function calculate() {
   mathresults.push(calculation);
   var mathresultsnocomma = mathresults.join(" ");
   document.querySelector("#results > p").innerHTML = mathresultsnocomma;
+  scrollToBottom();
+}
+
+function substracting() {
+  var firstnumber = Number(document.querySelector("#firstnumber").value);
+  var secondnumber = Number(document.querySelector("#secondnumber").value);
+  var calculation = firstnumber - secondnumber + "<br>";
+  mathresults.push(calculation);
+  var mathresultsnocomma = mathresults.join(" ");
+  document.querySelector("#results > p").innerHTML = mathresultsnocomma;
+  scrollToBottom();
+
+}
+
+function multiplying() {
+  var firstnumber = Number(document.querySelector("#firstnumber").value);
+  var secondnumber = Number(document.querySelector("#secondnumber").value);
+  var calculation = firstnumber * secondnumber + "<br>";
+  mathresults.push(calculation);
+  var mathresultsnocomma = mathresults.join(" ");
+  document.querySelector("#results > p").innerHTML = mathresultsnocomma;
+  scrollToBottom();
+
+}
+
+function dividing() {
+  var firstnumber = Number(document.querySelector("#firstnumber").value);
+  var secondnumber = Number(document.querySelector("#secondnumber").value);
+  var calculation = firstnumber / secondnumber + "<br>";
+  mathresults.push(calculation);
+  var mathresultsnocomma = mathresults.join(" ");
+  document.querySelector("#results > p").innerHTML = mathresultsnocomma;
+  scrollToBottom();
+
+}
+
+function clear() {
+  mathresults = [];
+  document.querySelector("#results > p").innerHTML = "";
+}
+
+// Taken from: https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
+
+function scrollToBottom() {
+  var div = document.querySelector("#results > p");
+  div.scrollIntoView(false);
 }
